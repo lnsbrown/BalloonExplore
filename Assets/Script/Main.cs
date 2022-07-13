8258c7f28d5f94f8c4758a70e23c1221e1d7769a
hjcenry <hejincheng1209@163.com>
2022-07-14 00:07:46 +0800

    init

---
+++ Assets/Script/Main.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 游戏主场景入口
+public class Main : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Start Game Main");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
